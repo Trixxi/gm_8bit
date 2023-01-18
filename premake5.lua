bdc9beab4db2ce63f7e693264fb5d82949c0e2ce
Marius <36795828+Trixxi@users.noreply.github.com>
2023-01-18 17:44:53 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -3,7 +3,7 @@ newoption({
 	description = "Sets the path to the garrysmod_common (https://github.com/danielga/garrysmod_common) directory",
 	value = "path to garrysmod_common directory"
 })
-
+externalincludedirs = sysincludedirs
 local gmcommon = assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
 	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
 include(gmcommon .. "/generator.v3.lua")
